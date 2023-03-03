3f81d83c3078e55dd134a8dea32d025935d63811
MANU N S <manuns1122@gmail.com>
2023-03-03 22:05:30 +0530

    "VoicePick is an intelligent object picking bot that allows users to select an object through voice recognition. The selected object is then used as a class object for the YOLO model, and OpenCV is utilized to detect the object. With VoicePick, users can easily and efficiently pick and detect objects with just their voice."
    "VoicePick" is an innovative project that utilizes advanced technologies such as voice recognition, YOLO (You Only Look Once) model, and OpenCV (Open Source Computer Vision Library) to develop an intelligent object picking bot. This project aims to make object detection and selection easier and more efficient for users by utilizing voice commands.
    
    With VoicePick, users can select any object they want through voice recognition. The selected object is then used as a class object for the YOLO model, which is a popular object detection algorithm. Once the object is selected, OpenCV is utilized to detect the object in real-time.
    
    VoicePick has numerous applications, such as in warehouses where workers can use voice commands to pick specific objects, or in museums where visitors can use their voice to learn more about specific exhibits. With VoicePick, object detection and selection become simpler and faster, making it an essential tool in many industries.

---
+++ utils/voiceTotexr.py
@@ -0,0 +1,18 @@
+import speech_recognition
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer()
+
+try:
+    with speech_recognition.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic,duration=5)
+        audio =recognizer.listen(mic)
+
+        text = recognizer.recognize_google(audio)
+        text=text.lower()
+
+        print(f"Recognized {text}")
+except speech_recognition.UnknownValueError():
+    recognizer = speech_recognition.Recognizer()
+
+
